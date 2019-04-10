647cfad9d2ea1986433839230f18effd9bb1536a
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2019-04-09 20:55:02 -0700

    added search and home page components

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import { Container, Button } from 'reactstrap';
+
+class Home extends Component {
+
+  state = {}
+
+  render() {
+    return (
+      <div>
+  
+      <Button>Homepage</Button>
+
+      </div>
+    )
+  }
+}
+
+export default Home;
